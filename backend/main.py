e1dd41cda069d55c037b7a0b5b7a3dd14b5f9588
pedro carmo <pasrcarmo@gmail.com>
2025-05-16 12:12:39 -0300

    ajustando CORS

--- backend/main.py
+++ backend/main.py
@@ -123,7 +123,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://listastop10-78sk.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
