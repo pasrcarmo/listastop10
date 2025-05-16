ae265d4fb8937b16a7a98d4010e0127c612f5066
pedro carmo <pasrcarmo@gmail.com>
2025-05-16 12:21:05 -0300

    ajustando biblioteca genai

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 import os
 from googleapiclient.discovery import build
 import json
